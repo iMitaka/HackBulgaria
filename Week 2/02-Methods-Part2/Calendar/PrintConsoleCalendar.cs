5d300723e3a294adede7bea2482a1878aeda6c39
iMitaka <dimitar.georgiev1992@gmail.com>
2015-11-29 17:20:25 +0200

    Update Week 2 Day 2 - Print Calendar Problem.

--- Week 2/02-Methods-Part2/Calendar/PrintConsoleCalendar.cs
+++ Week 2/02-Methods-Part2/Calendar/PrintConsoleCalendar.cs
@@ -5,29 +5,88 @@ public class PrintConsoleCalendar
 {
     public static void Main()
     {
-        PrintCalendar(11, 2015, new CultureInfo("en-US"));
+        PrintCalendar(11, 2015, new CultureInfo("bg-BG"));
+        Console.WriteLine();
     }
+
     public static void PrintCalendar(int month, int year, CultureInfo culture)
     {
-        Calendar myCal = culture.Calendar;
-        int daysInMounth = myCal.GetDaysInMonth(year, month);
-        Console.WriteLine(culture.DateTimeFormat.MonthNames[month - 1]);
-        Console.WriteLine();
-        foreach (var day in culture.DateTimeFormat.DayNames)
+        int daysInMonth = culture.Calendar.GetDaysInMonth(year, month);
+        string[,] calendarMatrix = new string[6, 7];
+        int nullDaysCount = 0;
+        int currentDay = 1;
+        int firstDayInMounth = 1;
+        string firstDay = new DateTime(year, month, 1).DayOfWeek.ToString();
+        string[] daysOfWeekNames = culture.DateTimeFormat.DayNames;
+
+        if (culture.DateTimeFormat.FirstDayOfWeek.ToString() == "Monday")
         {
-            Console.Write("{0,10} ", day);
-        }
-        Console.WriteLine();
-        Console.WriteLine();
-        for (int i = 1; i <= daysInMounth; i++)
-        {
-            Console.Write("{0,10} ", i);
-            if (i % 7 == 0)
+            for (int i = 1; i < daysOfWeekNames.Length; i++)
             {
-                Console.WriteLine("\n");
+                Console.Write("{0,10} ", daysOfWeekNames[i]);
+            }
+            Console.WriteLine("{0,10} ", daysOfWeekNames[0]);
+
+            switch (firstDay)
+            {
+                case "Monday": firstDayInMounth = 1; break;
+                case "Tuesday": firstDayInMounth = 2; break;
+                case "Wednesday": firstDayInMounth = 3; break;
+                case "Thursday": firstDayInMounth = 4; break;
+                case "Friday": firstDayInMounth = 5; break;
+                case "Saturday": firstDayInMounth = 6; break;
+                case "Sunday": firstDayInMounth = 7; break;
             }
         }
-        Console.WriteLine();
-        Console.WriteLine();
+        else 
+        {
+            for (int i = 0; i < daysOfWeekNames.Length; i++)
+            {
+                Console.Write("{0,10} ", daysOfWeekNames[i]);
+            }
+            Console.WriteLine();
+
+            switch (firstDay)
+            {
+                case "Monday": firstDayInMounth = 2; break;
+                case "Tuesday": firstDayInMounth = 3; break;
+                case "Wednesday": firstDayInMounth = 4; break;
+                case "Thursday": firstDayInMounth = 5; break;
+                case "Friday": firstDayInMounth = 6; break;
+                case "Saturday": firstDayInMounth = 7; break;
+                case "Sunday": firstDayInMounth = 1; break;
+            }
+        }
+
+        for (int week = 0; week < calendarMatrix.GetLength(0); week++)
+        {
+            for (int day = 0; day < calendarMatrix.GetLength(1); day++, currentDay++)
+            {
+                if (currentDay > daysInMonth + nullDaysCount)
+                {
+                    break;
+                }
+                if (currentDay >= firstDayInMounth)
+                {                   
+                    calendarMatrix[week, day] = string.Format("{0,10} ", currentDay - nullDaysCount);
+                }
+                else
+                {
+                    calendarMatrix[week, day] = string.Format("{0,10} ", " ");
+                    nullDaysCount++;
+                }
+            }
+        }
+
+        //print
+
+        for (int week = 0; week < calendarMatrix.GetLength(0); week++)
+        {
+            for (int day = 0; day < calendarMatrix.GetLength(1); day++)
+            {
+                Console.Write(calendarMatrix[week, day]);
+            }
+            Console.WriteLine();
+        }
     }
 }
