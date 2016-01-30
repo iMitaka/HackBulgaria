6ee035f7c321dcb3cb0e17445425540f201221c1
iMitaka <dimitar.georgiev1992@gmail.com>
2016-01-30 21:24:02 +0200

    Update ProgramMain.cs
    few refactoring code.

--- Week 9/02-ADO.NET/HackCompany.ConsoleApp/ProgramMain.cs
+++ Week 9/02-ADO.NET/HackCompany.ConsoleApp/ProgramMain.cs
@@ -1,39 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Configuration;
-using System.Data.SqlClient;
-using HackCompany.Data;
-
-namespace HackCompany.ConsoleApp
+﻿namespace HackCompany.ConsoleApp
 {
-    class Program
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Configuration;
+    using System.Data.SqlClient;
+    using HackCompany.Data;
+
+    public class ProgramMain
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            EmployeeUtility utility = new EmployeeUtility();
+            // EmployeeUtility utility = new EmployeeUtility();
 
             // Employee employee = new Employee() { Name = "Gosho Goshev", Id = 55, BirthDate = DateTime.Now.AddYears(-15), Email = "peshev@abv.bg", ManagerId = 2, DepartmentId = 1 };
 
-            // string test = employee.GetType().Name;
-            // Console.WriteLine(test);
+            // utility.AddToData(employee);
 
-            //utility.AddToData(employee);
-
-            //Console.WriteLine(employee.Id);
+            // Console.WriteLine(employee.Id);
 
             HackCompanyUtility myUtility = new HackCompanyUtility();
 
             var list = myUtility.Employees.GetAllEmplyees();
+
             foreach (var emp in list)
             {
                 Console.WriteLine(emp.Name);
             }
             Console.WriteLine();
 
+
             var employee = myUtility.Employees.GetEmployeeById(3);
+
             Console.WriteLine("{0} | {1} | {2} | {3} | {4} | {5}",employee.Id,employee.Name,employee.Email,employee.BirthDate.ToShortDateString(),employee.ManagerId,employee.DepartmentId);
         }
     }
